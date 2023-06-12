97a2185cec969dd952291cdc56126bd9a1e6a5ab
DaniloTorres2001 <dantvera@espol.edu.ec>
2023-06-11 19:58:19 -0500

    Fix ID

--- main.py
+++ main.py
@@ -1,99 +1,91 @@
 import ply.lex as lex
 
-# Crear los tokens para la siguiente sintaxis
-
-# SELECT * FROM Tabla
-# SELECT campo1, campo2 from Tabla1 where campo==1
-# SELECT campo1 as cedula from Datos where provincia<>"7"
-# print(consulta)
-# DELETE FROM datos WHERE id>1000
-# print("SELECT * FROM Tabla")
-
 # Diccionario de palabras reservadas
-reserved = {'alias': 'ALIAS',
-            'and': 'AND',
-            'begin': 'BEGIN',
-            'break': 'BREAK',
-            'case': 'CASE',
-            'class': 'CLASS',
-            'def': 'DEF',
-            'defined': 'DEFINED',
-            'do': 'DO',
-            'else': 'ELSE',
-            'elsif': 'ELSEIF',
-            'end': 'END_LOWER',
-            'END': 'END_UPPER',
-            'ensure': 'ENSURE',
-            'true': 'TRUE',
-            'false': 'FALSE',
-            'for': 'FOR',
-            'if': 'IF',
-            'in': 'IN',
-            'module': 'MODULE',
-            'next': 'NEXT',
-            'nil': 'NIL',
-            'not': 'NOT',
-            'or': 'OR',
-            'redo': 'REDO',
-            'rescue': 'RESCUE',
-            'retry': 'RETRY',
-            'return': 'RETURN',
-            'self': 'SELF',
-            'super': 'SUPER',
-            'then': 'THEN',
-            'undef': 'UNDEF',
-            'unless': 'UNLESS',
-            'until': 'UNTIL',
-            'when': 'WHEN',
-            'while': 'WHILE',
-            'yield': 'YIELD'
-            }
+reserved = {
+    'alias': 'ALIAS',
+    'and': 'AND',
+    'begin': 'BEGIN',
+    'break': 'BREAK',
+    'case': 'CASE',
+    'class': 'CLASS',
+    'def': 'DEF',
+    'defined': 'DEFINED',
+    'do': 'DO',
+    'else': 'ELSE',
+    'elsif': 'ELSEIF',
+    'end': 'END_LOWER',
+    'END': 'END_UPPER',
+    'ensure': 'ENSURE',
+    'true': 'TRUE',
+    'false': 'FALSE',
+    'for': 'FOR',
+    'if': 'IF',
+    'in': 'IN',
+    'module': 'MODULE',
+    'next': 'NEXT',
+    'nil': 'NIL',
+    'not': 'NOT',
+    'or': 'OR',
+    'redo': 'REDO',
+    'rescue': 'RESCUE',
+    'retry': 'RETRY',
+    'return': 'RETURN',
+    'self': 'SELF',
+    'super': 'SUPER',
+    'then': 'THEN',
+    'undef': 'UNDEF',
+    'unless': 'UNLESS',
+    'until': 'UNTIL',
+    'when': 'WHEN',
+    'while': 'WHILE',
+    'yield': 'YIELD'
+}
 
 # Sequencia de tokens, puede ser lista o tupla
 tokens = (
-        'ID',
-        'EQUALS',
-        'STR',
-        'INT',
-        'CONSTANT',
-        'FLOAT',
-        'SYMBOL',
-        'HASHAS',
-        #  Caracteres Especiales
-        'AT',
-        'COMMA',
-        'LBRACKET',
-        'RBRACKET',
-        'LPARENTHESIS',
-        'RPARENTHESIS',
-        'LCURLYBRACKET',
-        'RCURLYBRACKET',
-        'DOT',
-        'GLOBAL',
-        'COLON',
-        'SEMICOLON',
-        'PLUS',
-        'MINUS',
-        'MULTIPLY',
-        'DIVIDE',
-        'MODULO',
-        'EXPONENT',
-        'EQUAL',
-        'NOT_EQUAL',
-        'GREATER_THAN',
-        'LESS_THAN',
-        'GREATER_THAN_EQUAL',
-        'LESS_THAN_EQUAL',
-        'OP_AND',
-        'OP_OR',
-        'OP_NOT',
-        'BITWISE_AND',
-        'BITWISE_OR',
-        'BITWISE_XOR',
-        'BITWISE_NOT',
-        'SHIFT_LEFT',
-        'SHIFT_RIGHT'
-         ) + tuple(reserved.values())
+    'EQUALS',
+    'STR',
+    'INT',
+    'CONSTANT',
+    'FLOAT',
+    'SYMBOL',
+    'HASHAS',
+    # Caracteres Especiales
+    'AT',
+    'COMMA',
+    'LBRACKET',
+    'RBRACKET',
+    'LPARENTHESIS',
+    'RPARENTHESIS',
+    'LCURLYBRACKET',
+    'RCURLYBRACKET',
+    'DOT',
+    'GLOBAL',
+    'COLON',
+    'SEMICOLON',
+    'PLUS',
+    'MINUS',
+    'MULTIPLY',
+    'DIVIDE',
+    'MODULO',
+    'EXPONENT',
+    'EQUAL',
+    'NOT_EQUAL',
+    'GREATER_THAN',
+    'LESS_THAN',
+    'GREATER_THAN_EQUAL',
+    'LESS_THAN_EQUAL',
+    'OP_AND',
+    'OP_OR',
+    'OP_NOT',
+    'BITWISE_AND',
+    'BITWISE_OR',
+    'BITWISE_XOR',
+    'BITWISE_NOT',
+    'SHIFT_LEFT',
+    'SHIFT_RIGHT',
+    'ID',
+) + tuple(reserved.values())
 
 # Exp Regulares para tokens de símbolos
 t_EQUALS = r'='
@@ -137,27 +129,28 @@ t_SHIFT_LEFT = r'<<'
 t_SHIFT_RIGHT = r'>>'
 
 
-
-
-
 # Para contabilizar nro de líneas
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
 
-def t_ID(t):
-    r'[a-z][a-zA-Z_]\w+'
-    t.type = reserved.get(t.value.lower(), 'ID')
-    return t
-
-
+# Función para reconocer constantes
 def t_CONSTANT(t):
-    r'[A-Z][a-zA-Z_]\w+'
+    r'[A-Z][a-zA-Z_]\w*'
     t.type = reserved.get(t.value.lower(), 'CONSTANT')
     return t
 
 
+# Función para reconocer identificadores y palabras reservadas
+def t_ID(t):
+    r'[a-zA-Z_]\w*'
+    t.type = reserved.get(t.value.lower(), 'ID')
+    return t
+
+
+
+# Expresión regular para comentarios
 def t_COMMENTS(t):
     r'\#.*'
     pass
@@ -173,7 +166,7 @@ def t_error(t):
     t.lexer.skip(1)
 
 
-# Contruir analizador
+# Construir analizador
 lexer = lex.lex()
 
 # Testeando
@@ -184,7 +177,10 @@ x = 123.5 + (5 * 22)
 hola
 else
 return
-    '''
+if
+y23
+"hola"
+'''
 
 # Datos de entrada
 lexer.input(data)
